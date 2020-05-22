25d7edf5b528c908c24d273c769903b77ec780e7
Tomasz <kurek.tom@gmail.com>
2020-05-22 14:42:52 +0200

    Shoot winner bot code refactor

--- rule_based_bots/shoot_winner.py
+++ rule_based_bots/shoot_winner.py
@@ -11,6 +11,10 @@ class ShootWinner:
         self.max_ammo = 10
         self.bullet_speed = 700
         self.min_score_dif = 100
+        self.small_health = 5
+        self.close_dist = 125
+        self.medium_dist = 250
+        self.dist_to_shoot = 200
 
     def get_angle_with_speed_prediction(self, bot1, bot2, delta_time):
         x1 = bot1['x']
@@ -22,13 +26,13 @@ class ShootWinner:
         a = vx ** 2 + vy ** 2 - self.bullet_speed ** 2
         dx = (x2 - x1)
         dy = (y2 - y1)
-        beta = (atan2(dy, dx) * 180 / pi) % 360
+        angle = (atan2(dy, dx) * 180 / pi) % 360
         if a != 0:
             b = 2 * (vx * dx + vy * dy)
             c = dx * dx + dy * dy
             (t1, t2) = quadratic_equation(a, b, c)
             if t1 == 0:
-                return beta
+                return angle
             if t1 < 0:
                 t1 = 1000
             if t2 < 0:
@@ -36,11 +40,11 @@ class ShootWinner:
             if t1 > t2:
                 t1 = t2
             if t1 == 1000:
-                return beta
+                return angle
             dx = x2 - x1 + vx * (t1 + delta_time)
             dy = y2 - y1 + vy * (t1 + delta_time)
-            beta = (atan2(dy, dx) * 180 / pi) % 360
-        return beta
+            angle = (atan2(dy, dx) * 180 / pi) % 360
+        return angle
 
     def run(self):
         prev_time = time.clock()
@@ -57,43 +61,37 @@ class ShootWinner:
             smallest_hp_bot = bots[0]
             closest_bot = bots[0]
             max_score_bot = bots[0]
-            max_distance = 1000
-            smallest_hp = 21
-            max_score = -1
             score_dif = 0
-            for bot in bots:
-                if bot is not my:
-                    dist_to_enemy = dist(my, bot)
-                    current_bot_hp = bot['life']
-                    current_bot_score = bot['score']
-                    if current_bot_score > max_score and current_bot_hp != 0:
-                        score_dif = current_bot_score - max_score
-                        max_score = current_bot_score
-                        max_score_bot = bot
-                    if dist_to_enemy < max_distance and current_bot_hp != 0:
-                        max_distance = dist_to_enemy
-                        closest_bot = bot
-                    if (current_bot_hp < smallest_hp) and (current_bot_hp != 0):
-                        smallest_hp = current_bot_hp
-                        smallest_hp_bot = bot
+            for current_bot in bots:
+                if current_bot is not my:
+                    if current_bot['score'] > max_score_bot['score'] and current_bot['life'] != 0:
+                        score_dif = current_bot['score'] - max_score_bot['score']
+                        max_score_bot = current_bot
+                    if dist(my, current_bot) < dist(my, closest_bot) and current_bot['life'] != 0:
+                        closest_bot = current_bot
+                    if (current_bot['life'] < smallest_hp_bot['life']) and current_bot['life'] != 0:
+                        smallest_hp_bot = current_bot
 
-            if (my['life'] <= smallest_hp) or (score_dif < self.min_score_dif) or (smallest_hp < 5):
-                max_score_bot = smallest_hp_bot
+            if (my['life'] <= smallest_hp_bot['life']) or (score_dif < self.min_score_dif) \
+                    or (smallest_hp_bot['life'] < self.small_health and smallest_hp_bot['life'] != 0):
+                bot_to_target = smallest_hp_bot
+            else:
+                bot_to_target = max_score_bot
 
             y1 = my['y']
             x1 = my['x']
             x2 = closest_bot['x']
             y2 = closest_bot['y']
-            x3 = max_score_bot['x']
-            y3 = max_score_bot['y']
-            if dist(my, max_score_bot) < 250:
-                if dist(my, max_score_bot) < 125:
-                    angle_to_enemy = get_angle_between_bots(my, max_score_bot)
+            x3 = bot_to_target['x']
+            y3 = bot_to_target['y']
+            if dist(my, bot_to_target) < self.medium_dist:
+                if dist(my, bot_to_target) < self.close_dist:
+                    angle_to_enemy = get_angle_between_bots(my, bot_to_target)
                 else:
-                    angle_to_enemy = self.get_angle_with_speed_prediction(my, max_score_bot, delta_time)
+                    angle_to_enemy = self.get_angle_with_speed_prediction(my, bot_to_target, delta_time)
             else:
-                max_score_bot = closest_bot
-                if dist(my, closest_bot) < 125:
+                bot_to_target = closest_bot
+                if dist(my, closest_bot) < self.close_dist:
                     angle_to_enemy = get_angle_between_bots(my, closest_bot)
                 else:
                     angle_to_enemy = self.get_angle_with_speed_prediction(my, closest_bot, delta_time)
@@ -108,8 +106,8 @@ class ShootWinner:
             else:
                 dy = -1
 
-            if my['life'] > 5:
-                if dist(my, closest_bot) < 150:
+            if my['life'] > self.small_health:
+                if dist(my, closest_bot) < self.close_dist:
                     if (y1 - y2) > 0:
                         if (x1 - x2) > 0:
                             dx = 1
@@ -130,7 +128,8 @@ class ShootWinner:
             comm.move(dx, dy)
 
             shoot = False
-            if my['ammo'] >= self.max_ammo - 1 or dist(my, max_score_bot) < 200 or max_score_bot['life'] < 5:
+            if my['ammo'] >= self.max_ammo - 1 or dist(my, bot_to_target) < self.dist_to_shoot \
+                    or bot_to_target['life'] < self.small_health:
                 shoot = True
             comm.shoot(shoot)
 
